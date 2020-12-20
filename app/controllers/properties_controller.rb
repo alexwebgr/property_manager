69acb959863c33d4d575f93407a6f57cc833b07c
alexwebgr <websurfer992@gmail.com>
2020-12-20 12:15:16 +0200

    removed json responders from controllers - consistent styling across pages

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,7 +2,6 @@ class PropertiesController < ApplicationController
   before_action :set_property, only: [:edit, :update, :destroy]
 
   # GET /properties
-  # GET /properties.json
   def index
     @properties = current_user.properties.all
   end
@@ -17,7 +16,6 @@ class PropertiesController < ApplicationController
   end
 
   # POST /properties
-  # POST /properties.json
   def create
     local_params = property_params.clone
     local_params[:user_id] = current_user.id
@@ -26,35 +24,28 @@ class PropertiesController < ApplicationController
     respond_to do |format|
       if @property.save
         format.html { redirect_to properties_url, notice: 'Property was successfully created.' }
-        format.json { render :index, status: :created, location: @property }
       else
         format.html { render :new }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
         format.html { redirect_to properties_url, notice: 'Property was successfully updated.' }
-        format.json { render :index, status: :ok, location: @property }
       else
         format.html { render :edit }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
       format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/transaction_types_controller.rb
+++ app/controllers/transaction_types_controller.rb
@@ -2,13 +2,11 @@ class TransactionTypesController < ApplicationController
   before_action :set_transaction_type, only: [:show, :edit, :update, :destroy]
 
   # GET /transaction_types
-  # GET /transaction_types.json
   def index
     @transaction_types = TransactionType.all
   end
 
   # GET /transaction_types/1
-  # GET /transaction_types/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class TransactionTypesController < ApplicationController
   end
 
   # POST /transaction_types
-  # POST /transaction_types.json
   def create
     @transaction_type = TransactionType.new(transaction_type_params)
 
     respond_to do |format|
       if @transaction_type.save
         format.html { redirect_to @transaction_type, notice: 'Transaction type was successfully created.' }
-        format.json { render :show, status: :created, location: @transaction_type }
       else
         format.html { render :new }
-        format.json { render json: @transaction_type.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /transaction_types/1
-  # PATCH/PUT /transaction_types/1.json
   def update
     respond_to do |format|
       if @transaction_type.update(transaction_type_params)
         format.html { redirect_to @transaction_type, notice: 'Transaction type was successfully updated.' }
-        format.json { render :show, status: :ok, location: @transaction_type }
       else
         format.html { render :edit }
-        format.json { render json: @transaction_type.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /transaction_types/1
-  # DELETE /transaction_types/1.json
   def destroy
     @transaction_type.destroy
     respond_to do |format|
       format.html { redirect_to transaction_types_url, notice: 'Transaction type was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
